80292469aa36a84c8ce7e8959328de36c8844a49
shri <maneshriyash31@gmail.com>
2022-09-08 15:09:15 -0700

    new changes

--- core.py
+++ core.py
@@ -1,8 +1,68 @@
-s=[10,20,30,40]
-w=bytes(s)
-w[0]=100
-print(w)
+# s=[10,20,30,40]
+# w=bytes(s)
+# w[0]=100
+# print(w)
 
-tuple
+# l = tuple(range(10))
+# print(l)
+# d={}
+# d['a']='apple'
+# d['b']='banana'
+# print(d) 
+
+# list=[1,2,3,4]
+# b=bytes(list)
+# print(b[0])
+
+# s="durga\\software"
+# print(s)
+
+# a=45
+# d=20
+# c=a//d
+# print(c)
+
+# a,b=40,50
+# x=60 if a>b else 70
+# print(x)
+
+# a=int(input("Enter First Number:")) 
+# b=int(input("Enter Second Number:")) 
+# min=a if a<b else b 
+# print("Minimum Value:",min)
+
+# a=int(input("Enter First Number:")) 
+# b=int(input("Enter Second Number:")) 
+# c=int(input("Enter Third Number:")) 
+# max=a if a>b and a>c else b if b>c else c 
+# print("Maximum Value:",max)
+
+# import math as m
+# d=m.tan(5)
+# print(d)
+
+# print("The Sum:",int(input("Enter First Number:"))+int(input("Enter Second Number:"))) 
+
+# married=bool(input("Employee Married ?[TrueFalse]:"))
+# print("Employee Married ? :",married) 
+
+# a,b= [int(x) for x in input("Enter 2 numbers :").split()] 
+# print("Product is :", a*b) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
